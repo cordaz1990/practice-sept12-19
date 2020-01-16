d5a26f78295247fd1812f74ad3ba21c8eacb6c93
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:16:31 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,21 @@
-print('hello')
-print('wo
+def all_anagrams(filename):
+    d = {}
+    for line in open(filename):
+       word = line.strip().lower()
+       t = signature(word)
+      if t not in d:
+        d[t] = [word]
+      else:
+        d[t].append(word)
+    
+    return d
+    
+    
+ def all_anagrams(filename):
+     d = {}
+    for line in open(filename):
+      word = line.strip().lower()
+      t = signature(word)
+      d.setdefault(t,[]).append(word)
+   
+    return d
